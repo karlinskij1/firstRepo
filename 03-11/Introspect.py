7c975366a8f4fa4aa3f3192213fe5d0647274a26
Karlinskiy_v <karlinskij_v@itstep.academy>
2024-11-03 10:15:41 +0200

    03.11

--- 03-11/Introspect.py
+++ 03-11/Introspect.py
@@ -1,1 +1,23 @@
-print("Hello world")
+import requests
+
+def first_func():
+    pass
+
+class Human:
+    pass
+
+rs = requests
+
+first_f = first_func
+vlad = Human
+
+print(type(rs))
+print(type(first_f))
+print(type(vlad))
+
+print(requests.__name__)
+print(rs.__name__)
+print(first_f.__name__)
+print(vlad.__name__)
+
+print(__name__)
---
+++ 03-11/Introspect2.py
@@ -0,0 +1,11 @@
+data = "string"
+print(hasattr(data, "reverse"))
+print(hasattr(data, "index"))
+
+print(f'!!! {data.startswith("st")} !!!')
+
+starts = getattr(data, "startswith", 'st')
+
+
+print(type(starts))
+
